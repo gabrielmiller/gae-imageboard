58f83f81ea15c470cadfa53969e176849a3f5cc5
unknown <Admin@SLOPPYCOCKS.(none)>
2012-05-13 14:39:25 -0400

    Image posting and serving both work. Need to play with the CSS and maybe the markup.

--- imgboard.py
+++ imgboard.py
@@ -3,7 +3,6 @@ import webapp2
 import jinja2
 
 from google.appengine.ext import db
-
 from google.appengine.api import images
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -23,7 +22,6 @@ class Handler(webapp2.RequestHandler):
 class Postedimage(db.Model):
 	title = db.StringProperty(required=True)
 	upfile = db.BlobProperty(required=True)
-	imgkey = db.BlobKey(auto_now_add=True)
 	text = db.TextProperty()
 	createdate = db.DateTimeProperty(auto_now_add=True)
 
@@ -33,9 +31,6 @@ class MainPage(Handler):
 		self.render("index.html", title=title, upfile=upfile, text=text, error=error, postedimages=postedimages)
 
 	def get(self):
-#		self.response.headers['Content-Type'] = 'text/html'
-#		self.response.out.write('ImageApp!')
-#		self.render("index.html")
 		self.render_page()
 
 	def post(self):
@@ -44,7 +39,6 @@ class MainPage(Handler):
 		text = self.request.get("text")
 
 		if title and text and upfile:
-#			self.write("it worked!")
 			z = Postedimage(title=title, upfile=upfile, text=text)
 			z.put()
 			self.redirect("/")
@@ -53,24 +47,16 @@ class MainPage(Handler):
 			error = "Error: You must specify at least a title and file."
 			self.render_page(title, upfile, text, error)
 
-#class GetImage(Handler):
-#    def get(self):
-#    	title = self.request.get('title')
-#    	upfile = getUpfile(title)
-#    	if (Postedimage and Postedimage.title):
-#       		self.response.headers['Content-Type'] = 'image/jpeg'
-#       		self.response.out.write(Postedimage.upfile)
-#        else:
-#            self.redirect('/#')
-#
-#    def getUpfile(title):
-#    	result = db.GqlQuery("SELECT * FROM Postedimages WHERE title = :1 LIMIT 1", title).fetch(1)
-#    	if (len(result) > 0):
-#        	return result[0]
-#    	else:
-#        	return None
+class GetImage (Handler):
+     def get(self):
+       relayimage = db.get(self.request.get("img_id"))
+       if relayimage.upfile:
+           self.response.headers['Content-Type'] = "image/jpg"
+           self.response.out.write(relayimage.upfile)
+       else:
+           self.error(404)
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)#,('/image', GetImage)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage),('/img', GetImage)], debug=True)#,('/image', GetImage)], debug=True)
 
 #apps_binding.append(('/image', GetImage))
 #application = webapp.WSGIApplication(apps_binding, debug=True)
